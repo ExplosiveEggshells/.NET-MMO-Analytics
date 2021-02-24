65f41a1a79c1dcb7b7aea24f151fc1796e2f9b43
J-Erwin <johnny.composer.man@gmail.com>
2021-02-24 16:37:48 -0600

    Fixed Event handlers and other things

--- RogersErwin_Assign3/Form1.Designer.cs
+++ RogersErwin_Assign3/Form1.Designer.cs
@@ -43,9 +43,20 @@
             this.PRSLMinLevelUpDown = new System.Windows.Forms.NumericUpDown();
             this.PRSLMaxLevelUpDown = new System.Windows.Forms.NumericUpDown();
             this.PRSLRoleLabel = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ClassComboBox = new System.Windows.Forms.ComboBox();
+            this.ServerComboBox = new System.Windows.Forms.ComboBox();
+            this.AllClassTypeQueryButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PercentageOfRaceComboBox = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.MaxLvlPlayersQueryButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PRSLMinLevelUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PRSLMaxLevelUpDown)).BeginInit();
@@ -56,25 +67,25 @@
             this.OutputListBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OutputListBox.FormattingEnabled = true;
             this.OutputListBox.ItemHeight = 16;
-            this.OutputListBox.Location = new System.Drawing.Point(359, 42);
+            this.OutputListBox.Location = new System.Drawing.Point(464, 42);
             this.OutputListBox.Name = "OutputListBox";
-            this.OutputListBox.Size = new System.Drawing.Size(996, 516);
+            this.OutputListBox.Size = new System.Drawing.Size(926, 612);
             this.OutputListBox.TabIndex = 0;
             // 
             // GuildsPerTypeLabel
             // 
             this.GuildsPerTypeLabel.AutoSize = true;
-            this.GuildsPerTypeLabel.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GuildsPerTypeLabel.Location = new System.Drawing.Point(12, 300);
+            this.GuildsPerTypeLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuildsPerTypeLabel.Location = new System.Drawing.Point(0, 337);
             this.GuildsPerTypeLabel.Name = "GuildsPerTypeLabel";
-            this.GuildsPerTypeLabel.Size = new System.Drawing.Size(175, 22);
+            this.GuildsPerTypeLabel.Size = new System.Drawing.Size(158, 18);
             this.GuildsPerTypeLabel.TabIndex = 1;
             this.GuildsPerTypeLabel.Text = "Guilds Per Type";
             // 
             // GuildsPerTypeComboBox
             // 
             this.GuildsPerTypeComboBox.FormattingEnabled = true;
-            this.GuildsPerTypeComboBox.Location = new System.Drawing.Point(15, 342);
+            this.GuildsPerTypeComboBox.Location = new System.Drawing.Point(20, 375);
             this.GuildsPerTypeComboBox.Name = "GuildsPerTypeComboBox";
             this.GuildsPerTypeComboBox.Size = new System.Drawing.Size(168, 21);
             this.GuildsPerTypeComboBox.TabIndex = 2;
@@ -83,20 +94,20 @@
             // GuildsPerTypeCBLabel
             // 
             this.GuildsPerTypeCBLabel.AutoSize = true;
-            this.GuildsPerTypeCBLabel.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GuildsPerTypeCBLabel.Location = new System.Drawing.Point(12, 322);
+            this.GuildsPerTypeCBLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuildsPerTypeCBLabel.Location = new System.Drawing.Point(17, 355);
             this.GuildsPerTypeCBLabel.Name = "GuildsPerTypeCBLabel";
-            this.GuildsPerTypeCBLabel.Size = new System.Drawing.Size(44, 17);
+            this.GuildsPerTypeCBLabel.Size = new System.Drawing.Size(40, 16);
             this.GuildsPerTypeCBLabel.TabIndex = 1;
             this.GuildsPerTypeCBLabel.Text = "Type";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(14, 62);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.Size = new System.Drawing.Size(42, 16);
             this.label1.TabIndex = 10;
             this.label1.Text = "Class";
             // 
@@ -105,12 +116,11 @@
             this.groupBox1.Controls.Add(this.FulfillRadioButton1);
             this.groupBox1.Controls.Add(this.FulfillRadioButton2);
             this.groupBox1.Controls.Add(this.FulfillRadioButton0);
-            this.groupBox1.Location = new System.Drawing.Point(13, 403);
+            this.groupBox1.Location = new System.Drawing.Point(13, 450);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(340, 47);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             // 
             // FulfillRadioButton1
             // 
@@ -154,17 +164,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(133, 62);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(155, 37);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.Size = new System.Drawing.Size(48, 16);
             this.label2.TabIndex = 11;
             this.label2.Text = "Server";
             // 
             // PRSLRolesComboBox
             // 
             this.PRSLRolesComboBox.FormattingEnabled = true;
-            this.PRSLRolesComboBox.Location = new System.Drawing.Point(16, 232);
+            this.PRSLRolesComboBox.Location = new System.Drawing.Point(20, 247);
             this.PRSLRolesComboBox.Name = "PRSLRolesComboBox";
             this.PRSLRolesComboBox.Size = new System.Drawing.Size(121, 21);
             this.PRSLRolesComboBox.TabIndex = 4;
@@ -173,7 +183,7 @@
             // PRSLServersComboBox
             // 
             this.PRSLServersComboBox.FormattingEnabled = true;
-            this.PRSLServersComboBox.Location = new System.Drawing.Point(158, 232);
+            this.PRSLServersComboBox.Location = new System.Drawing.Point(162, 247);
             this.PRSLServersComboBox.Name = "PRSLServersComboBox";
             this.PRSLServersComboBox.Size = new System.Drawing.Size(121, 21);
             this.PRSLServersComboBox.TabIndex = 4;
@@ -181,7 +191,7 @@
             // 
             // PRSLMinLevelUpDown
             // 
-            this.PRSLMinLevelUpDown.Location = new System.Drawing.Point(79, 265);
+            this.PRSLMinLevelUpDown.Location = new System.Drawing.Point(83, 280);
             this.PRSLMinLevelUpDown.Maximum = new decimal(new int[] {
             60,
             0,
@@ -204,7 +214,7 @@
             // 
             // PRSLMaxLevelUpDown
             // 
-            this.PRSLMaxLevelUpDown.Location = new System.Drawing.Point(221, 265);
+            this.PRSLMaxLevelUpDown.Location = new System.Drawing.Point(225, 280);
             this.PRSLMaxLevelUpDown.Maximum = new decimal(new int[] {
             60,
             0,
@@ -228,26 +238,17 @@
             // PRSLRoleLabel
             // 
             this.PRSLRoleLabel.AutoSize = true;
-            this.PRSLRoleLabel.Location = new System.Drawing.Point(15, 211);
+            this.PRSLRoleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PRSLRoleLabel.Location = new System.Drawing.Point(19, 226);
             this.PRSLRoleLabel.Name = "PRSLRoleLabel";
-            this.PRSLRoleLabel.Size = new System.Drawing.Size(29, 13);
+            this.PRSLRoleLabel.Size = new System.Drawing.Size(37, 16);
             this.PRSLRoleLabel.TabIndex = 6;
             this.PRSLRoleLabel.Text = "Role";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(155, 211);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 18);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "Server";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(44, 267);
+            this.label4.Location = new System.Drawing.Point(48, 282);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(24, 13);
             this.label4.TabIndex = 6;
@@ -256,18 +257,146 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(186, 267);
+            this.label5.Location = new System.Drawing.Point(190, 282);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(27, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "Max";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(0, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(358, 18);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "All Class Type from a Single Server";
+            // 
+            // ClassComboBox
+            // 
+            this.ClassComboBox.FormattingEnabled = true;
+            this.ClassComboBox.Location = new System.Drawing.Point(12, 57);
+            this.ClassComboBox.Name = "ClassComboBox";
+            this.ClassComboBox.Size = new System.Drawing.Size(124, 21);
+            this.ClassComboBox.TabIndex = 14;
+            // 
+            // ServerComboBox
+            // 
+            this.ServerComboBox.FormattingEnabled = true;
+            this.ServerComboBox.Location = new System.Drawing.Point(157, 56);
+            this.ServerComboBox.Name = "ServerComboBox";
+            this.ServerComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ServerComboBox.TabIndex = 15;
+            // 
+            // AllClassTypeQueryButton
+            // 
+            this.AllClassTypeQueryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AllClassTypeQueryButton.Location = new System.Drawing.Point(284, 55);
+            this.AllClassTypeQueryButton.Name = "AllClassTypeQueryButton";
+            this.AllClassTypeQueryButton.Size = new System.Drawing.Size(67, 23);
+            this.AllClassTypeQueryButton.TabIndex = 16;
+            this.AllClassTypeQueryButton.Text = "Search";
+            this.AllClassTypeQueryButton.UseVisualStyleBackColor = true;
+            this.AllClassTypeQueryButton.Click += new System.EventHandler(this.AllClassTypeQueryButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(0, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(448, 18);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Percentage of Each Race From a Single Server";
+            // 
+            // PercentageOfRaceComboBox
+            // 
+            this.PercentageOfRaceComboBox.FormattingEnabled = true;
+            this.PercentageOfRaceComboBox.Location = new System.Drawing.Point(157, 145);
+            this.PercentageOfRaceComboBox.Name = "PercentageOfRaceComboBox";
+            this.PercentageOfRaceComboBox.Size = new System.Drawing.Size(121, 21);
+            this.PercentageOfRaceComboBox.TabIndex = 19;
+            this.PercentageOfRaceComboBox.SelectedIndexChanged += new System.EventHandler(this.PercentageOfRaceComboBox_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(155, 126);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(159, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Server";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(0, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(358, 36);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "All Role Types from a Single Server\r\n       Within a Level Range";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(0, 429);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(448, 18);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "All Players Who Could Fill a Role But Aren\'t";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(0, 545);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(458, 18);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Percentage of Max Level Players in All Guilds";
+            // 
+            // MaxLvlPlayersQueryButton
+            // 
+            this.MaxLvlPlayersQueryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxLvlPlayersQueryButton.Location = new System.Drawing.Point(109, 566);
+            this.MaxLvlPlayersQueryButton.Name = "MaxLvlPlayersQueryButton";
+            this.MaxLvlPlayersQueryButton.Size = new System.Drawing.Size(215, 42);
+            this.MaxLvlPlayersQueryButton.TabIndex = 25;
+            this.MaxLvlPlayersQueryButton.Text = "Show Results";
+            this.MaxLvlPlayersQueryButton.UseVisualStyleBackColor = true;
+            this.MaxLvlPlayersQueryButton.Click += new System.EventHandler(this.MaxLvlPlayersQueryButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1402, 583);
+            this.ClientSize = new System.Drawing.Size(1402, 672);
+            this.Controls.Add(this.MaxLvlPlayersQueryButton);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.PercentageOfRaceComboBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.AllClassTypeQueryButton);
+            this.Controls.Add(this.ServerComboBox);
+            this.Controls.Add(this.ClassComboBox);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.PRSLRoleLabel);
@@ -310,9 +439,20 @@
         private System.Windows.Forms.NumericUpDown PRSLMinLevelUpDown;
         private System.Windows.Forms.NumericUpDown PRSLMaxLevelUpDown;
         private System.Windows.Forms.Label PRSLRoleLabel;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox ClassComboBox;
+        private System.Windows.Forms.ComboBox ServerComboBox;
+        private System.Windows.Forms.Button AllClassTypeQueryButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox PercentageOfRaceComboBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button MaxLvlPlayersQueryButton;
     }
 }
 
